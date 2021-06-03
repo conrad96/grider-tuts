6f1aeba601f44ed373df42e7ce23ab2526870851
conrad96 <conrad.mugisha@gmail.com>
2021-06-03 05:25:10 +0300

    chain axios requests

--- axios.js
+++ axios.js
@@ -1,11 +1,33 @@
 const axios = require('axios');
 
-axios.get('https://swapi.dev/api/planets'). 
-then((response)=> {
-    let planets = response.data.results;
+// axios.get('https://swapi.dev/api/planets'). 
+// then((response)=> {
+//     let planets = response.data.results;
     
-    for(let planet of planets)
+//     for(let planet of planets)
+//     {
+//         console.log(planet.name);
+//     }
+// })
+
+//Axios chaining
+
+showPlanets = (response) => {
+    let data = response.data.results;
+    let next = response.data.next;
+
+    for(let planet of data)
     {
         console.log(planet.name);
     }
-})
+
+    return fetchNext(next);
+}
+
+fetchNext = (url) => {
+    return axios.get(url);
+}
+
+axios.get('https://swapi.dev/api/planets').
+then(showPlanets).
+then(showPlanets)
