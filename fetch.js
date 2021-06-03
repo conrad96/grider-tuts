adb89e867235fa18d14d0fbc5d3979050c533355
conrad96 <conrad.mugisha@gmail.com>
2021-06-03 04:54:06 +0300

    chaining fetch requests

--- fetch.js
+++ fetch.js
@@ -1,15 +1,41 @@
 const fetch = require('node-fetch');
 
-fetch('https://swapi.dev/api/planets').then((response)=> {
-    //throw error
-    if(response.status != 200) throw new Error(`Status code: ${response.status}`);
+// fetch('https://swapi.dev/api/planets').then((response)=> {
+//     //throw error
+//     if(response.status != 200) throw new Error(`Status code: ${response.status}`);
 
-    response.json().then((data)=> {
-        for(let planet of data.results)
-        {
-            console.log(planet.name);
-        }
-    })
-}).catch((error)=> {
-    console.log(error);
-})
+//     response.json().then((data)=> {
+//         for(let planet of data.results)
+//         {
+//             console.log(planet.name);
+//         }
+//     })
+// }).catch((error)=> {
+//     console.log(error);
+// })  
+
+//Fetch chaining
+
+showPlanets = (data) => {   
+    let next = data.next; 
+    for(let planet of data.results)
+    {
+        console.log(planet.name);
+    }
+    
+    return fetch(next);
+}
+
+parseJson = (responseBody) => {
+    return responseBody.json();
+}
+
+fetchNext = (data) => {
+    showPlanets(data);
+}
+
+fetch('https://swapi.dev/api/planets'). 
+then(parseJson).
+then(showPlanets).
+then(parseJson).
+then(fetchNext)
