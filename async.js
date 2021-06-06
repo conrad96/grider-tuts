89450ad6ba2cd479f5aed9eae40a3609f70149c5
conrad96 <conrad.mugisha@gmail.com>
2021-06-07 01:39:42 +0300

    using await with async functions

--- async.js
+++ async.js
@@ -1,15 +1,27 @@
-async function add(x, y)
-{
-    if(typeof x !== 'number' || typeof y !== 'number') throw 'x and y must be numbers';
+// async function add(x, y)
+// {
+//     if(typeof x !== 'number' || typeof y !== 'number') throw 'x and y must be numbers';
+
+const { default: axios } = require("axios");
+
     
-    return x + y;
+//     return x + y;
+// }
+
+// // let value = add(10, '5');
+// // console.log(value);
+
+// add(10, '5').then((value)=> {
+//     console.log('SUM: '+ value);
+// }).catch((error)=> {
+//     console.log('Error: '+ error);
+// })
+
+async function getPlanets()
+{
+    //await waits till the promise is done resolving
+    const resp = await axios.get('https://swapi.dev/api/planets');
+    console.log(resp.data);
 }
 
-// let value = add(10, '5');
-// console.log(value);
-
-add(10, '5').then((value)=> {
-    console.log('SUM: '+ value);
-}).catch((error)=> {
-    console.log('Error: '+ error);
-})
+getPlanets();
