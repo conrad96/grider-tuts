9e29b850025d4ba039730b4eebc6337e156a9bc6
conrad96 <conrad.mugisha@gmail.com>
2021-06-10 23:30:43 +0300

    countdown timer, get duration us etick every sec subtracting timer, pause by clearing Interval, how to go about the this in function calls

--- timer/index.js
+++ timer/index.js
@@ -7,15 +7,26 @@ class Timer
         this.pauseButton = pauseButton;
 
         this.startButton.addEventListener('click', this.start);
+        this.pauseButton.addEventListener('click', this.pause);
     }
 
-    start()
-    {
-        console.log('started');
+    start = () => {
+        this.tick();
+        this.interval = setInterval(this.tick, 1000);
+    }
+
+    tick = () => {
+        let countdown = parseInt(this.durationInput.value);        
+        if(this.durationInput.value > 0) this.durationInput.value = countdown - 1;
+    }
+
+    pause = () => {
+        clearInterval(this.interval);
     }
 }
 
 let startBtn = document.querySelector('#start');
 let pauseBtn = document.querySelector('#pause');
+let duration = document.querySelector('#duration');
 
-const obj = new Timer(30, startBtn, pauseBtn);
+const obj = new Timer(duration, startBtn, pauseBtn);
