25728c41c12eda0a8530cbb75ae3e4ede268b565
conrad96 <conrad.mugisha@gmail.com>
2021-06-17 09:48:29 +0300

    add dropdown html to autocomplete, show movies search results in dropdown

--- movies/index.js
+++ movies/index.js
@@ -13,21 +13,38 @@ let fetchData = async(movie) => {
     return response.data.Search;
 }
 
+let root = document.querySelector('#auto-complete');
+
+root.innerHTML = `
+<label>Search Movie</label><p />
+<input type="text" name="search" />
+<div class="dropdown">
+    <div class="dropdown-menu">
+        <div class="dropdown-content results">            
+        </div>    
+    </div>
+</div>
+`;
+
 let input = document.querySelector('input');
+let dropdown = document.querySelector('.dropdown');
+let results = document.querySelector('.results');
 
 let onInput = async evt => {
     const movies = await fetchData(evt.target.value);
-    
+
+    dropdown.classList.add('is-active');
+
     for(let movie of movies)
     {
-        const div = document.createElement('div');
-        div.innerHTML = `
-        <img src="${movie.Poster}" style="width: 300px; height: 250px;" />
-        <h1>${movie.Title}</h1>
-        <h3>${movie.Year}</h3>
-        `;
+        const item = document.createElement('a');
+        item.classList.add('dropdown-item');
 
-        document.querySelector('#movie-canvas').appendChild(div);
+        item.innerHTML = `
+        <img src="${movie.Poster}" />
+        ${movie.Title}`;
+
+        results.appendChild(item);
     }
 }
 
