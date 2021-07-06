2d65a2e526225f8daa36392bdfe3ebfe5c52f91d
conrad96 <conrad.mugisha@gmail.com>
2021-07-06 05:30:01 +0300

    decrypt message in card panel

--- message/index.js
+++ message/index.js
@@ -1,6 +1,18 @@
 let form = document.querySelector('form');
 let input = document.querySelector('#message');
 let msgOut = document.querySelector('#msg-out');
+const {hash} = window.location;
+
+const message = atob(hash.replace('#', ''));
+
+if(message)
+{
+    document.querySelector('#link-output').classList.add('hide');
+    document.querySelector('#form-panel').classList.add('hide');
+    document.querySelector('#decrypt-output').classList.remove('hide');
+
+    document.querySelector('h1').innerHTML = message;
+}
 
 form.addEventListener('submit', (evt)=> {
     evt.preventDefault();    
@@ -12,4 +24,6 @@ form.addEventListener('submit', (evt)=> {
     msgOut.select();
 
     document.querySelector('#link-output').classList.remove('hide');
+    document.querySelector('#form-panel').classList.add('hide');
+    console.log(window.location);
 })
